43a6b6ee13a7a1b7a9d1b78ff378dc617f63e768
Adolfo Neto <adolfo.usp@gmail.com>
2020-10-13 12:18:52 -0300

    Tests for 5% discount (two copies) added.

--- test/harry_potter_bookstore_test.exs
+++ test/harry_potter_bookstore_test.exs
@@ -19,4 +19,26 @@ defmodule HarryPotterBookstoreTest do
     shopping_cart = [{1, 2}, {2, 3}]
     assert HarryPotterBookstore.full_price(shopping_cart, 8) == 40
   end
+
+  test "count number of two copies of different books in a shopping cart" do
+    shopping_cart = [{1, 1}]
+    assert HarryPotterBookstore.two_copies(shopping_cart) == 0
+
+    shopping_cart = [{1, 1}, {2, 1}]
+    assert HarryPotterBookstore.two_copies(shopping_cart) == 2
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}]
+    assert HarryPotterBookstore.two_copies(shopping_cart) == 6
+  end
+
+  test "calculate 5% discount for two books" do
+    shopping_cart = [{1, 1}]
+    assert HarryPotterBookstore.discount_two_copies(shopping_cart, 8) == 8
+
+    shopping_cart = [{1, 1}, {2, 1}]
+    assert HarryPotterBookstore.discount_two_copies(shopping_cart, 8) == 16 * 0.95
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}]
+    assert HarryPotterBookstore.discount_two_copies(shopping_cart, 8) == 8 + 6 * 8 * 0.95
+  end
 end
