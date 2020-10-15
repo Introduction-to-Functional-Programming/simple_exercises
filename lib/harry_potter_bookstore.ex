dbe36986cd07bd0ed15d15c68e969a4010a16c97
Turatti <mvbturatti@gmail.com>
2020-10-14 22:52:29 -0300

    6 exercícios

--- lib/harry_potter_bookstore.ex
+++ lib/harry_potter_bookstore.ex
@@ -22,4 +22,51 @@ defmodule HarryPotterBookstore do
   defp rem_by_2(quantidade) do
     rem(quantidade, 2)
   end
+
+  def shopping_cart_map({book, quantity}) do
+    %{book => quantity}
+  end
+
+  defstruct [{book,quantity}]
+  def shopping_cart_keyword(:livro, quantidade) do
+    [{livro:quantidade}]
+  end
+
+  def discount_three_copies(shopping_cart, price) do
+    quantidade = number_books(shopping_cart)
+    ((div(quantidade,3) * 0.90 * 3) + (rem(quantidade,3))) * price
+  end
+
+  def discount_four_copies(shopping_cart, price) do
+    quantidade = number_books(shopping_cart)
+    ((div(quantidade,4) * 0.80 * 4) + (rem(quantidade,4))) * price
+  end
+
+  def discount_five_copies(shopping_cart, price) do
+    quantidade = number_books(shopping_cart)
+    ((div(quantidade,5) * 0.75 * 5) + (rem(quantidade,5))) * price
+  end
+
+  def discount_all(shopping_cart, price) do
+    quantidade = number_books(shopping_cart)
+    cond do
+      quantidade == 6 ->
+        ((4 * 0.80) + 2) * price
+      true ->
+        disc_helper(quantidade, 5)
+    end
+  end
+
+  defp disc_helper(quantidade, valor) do
+    cond do 
+      valor == 5 ->
+        (div(quantidade, 5) * 0.75 * 5) + disc_helper(rem(quantidade,5), valor - 1)
+      valor == 4 ->
+        (div(quantidade, 4) * 0.80 * 4) + disc_helper(rem(quantidade,4), valor - 1)
+      valor == 3 ->
+        (div(quantidade, 3) * 0.90 * 3) + disc_helper(rem(quantidade,3), valor - 1)
+      valor == 2 ->
+        (div(quantidade, 2) * 0.95 * 2) + rem(quantidade, 2)
+    end
+  end
 end
