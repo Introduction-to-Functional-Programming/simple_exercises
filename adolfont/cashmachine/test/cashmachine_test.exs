e661b5378d1a33115d5e74d23829335d952c5fdf
Adolfo Neto <adolfo.usp@gmail.com>
2020-10-29 16:22:03 -0300

    Version without enum reduce while

--- adolfont/cashmachine/test/cashmachine_test.exs
+++ adolfont/cashmachine/test/cashmachine_test.exs
@@ -9,10 +9,12 @@ defmodule CashmachineTest do
   test "2 dollars give me a 2-dollar bill" do
     assert CashMachine.withdraw(2) == %{2 => 1}
   end
-end
 
-# IO.gets("Digite o valor: ")
-# |> String.trim()
-# |> String.to_integer()
-# |> CashMachine.withdraw_reduce()
-# |> IO.inspect()
+  test "187 dollars gives me one of each bill" do
+    assert CashMachine.withdraw(187) == %{2 => 1, 5 => 1, 10 => 1, 20 => 1, 50 => 1, 100 => 1}
+  end
+
+  test "200 dollars gives me two 100 dollar bills" do
+    assert CashMachine.withdraw(200) == %{100 => 2}
+  end
+end
