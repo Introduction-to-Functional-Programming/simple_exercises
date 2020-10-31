a764d18a348a954e658dc1ad09e9ffe730b24785
Adolfo Neto <adolfo.usp@gmail.com>
2020-10-31 11:11:50 -0300

    Showing how to call an Elixir function from an Erlang file.

---
+++ adolfont/recursion/lib/mymodule.ex
@@ -0,0 +1,3 @@
+defmodule MyModule do
+  def my_function(), do: 1
+end
--- adolfont/recursion/lib/recursion.erl
+++ adolfont/recursion/lib/recursion.erl
@@ -1,6 +1,6 @@
--module recursion.
+-module(recursion).
 
--export [fac/1, fac2/1, fib/1, fib2/1].
+-export([fac/1, fac2/1, fib/1, fib2/1]).
 
 fac(1) ->
     1;
@@ -9,26 +9,25 @@ fac(N) ->
 
 fac2(N) ->
     fac2(N, 1).
+
 fac2(1, Current) ->
     Current;
 fac2(N, Current) ->
     fac2(N - 1, N * Current).
 
 fib(1) ->
-    1;
+    'Elixir.MyModule':my_function();
 fib(2) ->
     1;
 fib(N) ->
     fib(N - 1) + fib(N - 2).
 
-
 % Comparar a partir de 40
-fib2(N) when is_integer(N) andalso (N > 0) ->
+fib2(N) when is_integer(N) andalso N > 0 ->
     fib2(N, 0, 1).
 
 fib2(1, _Acc1, Acc2) ->
     Acc2;
 fib2(N, Acc1, Acc2) ->
     fib2(N - 1, Acc2, Acc1 + Acc2).
-
 
