a3f197839ab32bbfb2bf0bbf8310c80ea01ab712
Adolfo Neto <adolfo.usp@gmail.com>
2020-11-23 09:59:17 -0300

    Small refactoring.

--- cristineguadelupe/streams/lib/streams.ex
+++ cristineguadelupe/streams/lib/streams.ex
@@ -1,22 +1,23 @@
 defmodule Streams do
-
-  def fibonacci() do
-    Stream.unfold({0, 1}, fn {last, next} -> {last, {next, last + next}} end)
-  end
   def fibonacci_of(n) do
-    fibonacci()
+    fibonacci_generator()
     |> Enum.at(n)
   end
+
   def fibonacci_sequence(n) do
-    fibonacci()
+    fibonacci_generator()
     |> Enum.take(n)
   end
 
+  defp fibonacci_generator() do
+    Stream.unfold({0, 1}, fn {last, next} -> {last, {next, last + next}} end)
+  end
+
   def factorial(0), do: 1
+
   def factorial(n) do
     Stream.unfold({1, 2}, fn {last, next} -> {last, {next * last, next + 1}} end)
     |> Enum.take(n)
     |> List.last()
   end
-
 end
