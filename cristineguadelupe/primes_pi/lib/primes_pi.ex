fad9bf0b0c902677f812a415b8665d7dea6d6df1
Cristine Guadelupe <cristineguadelupe@me.com>
2020-11-25 22:10:51 -0300

    Remove old code and add a right test

--- cristineguadelupe/primes_pi/lib/primes_pi.ex
+++ cristineguadelupe/primes_pi/lib/primes_pi.ex
@@ -1,41 +1,21 @@
 defmodule PrimesPi do
 
-  @max_digits_constant 4
-
   def biggest_sequence(number) do
     number
     |> Enum.join()
     # |> String.slice(2..-1)
-    |> all_slices()
-    |> all_primes()
   end
 
-  def all_slices(number) do
-    number
-    |> max_digits()
-    |> Stream.unfold(fn
-      0 -> nil
-      n -> {slices(number, n), n-1}
-    end)
-    |> Enum.to_list()
-    |> List.flatten()
-  end
-
-  def all_primes(slices) do
-    slices
-    |> Enum.filter(&is_prime?/1)
-  end
-
-  def all_primes_ordered(number) do
+  def prepare_combinations(number) do
     number
     |> String.graphemes()
     |> Stream.unfold(fn n -> {Enum.take(n, 4), tl(n)} end)
     |> Enum.take(20)
-    |> Enum.map(&combinations/1)
+    |> Enum.map(&generate_combinations/1)
     |> List.flatten()
   end
 
-  def combinations(number) do
+  def generate_combinations(number) do
     number
     |> Enum.reverse()
     |> Stream.unfold(fn n -> {Enum.take(n, length(n)), tl(n)} end)
@@ -44,6 +24,11 @@ defmodule PrimesPi do
     |> Enum.map(&Enum.reverse/1)
     |> Enum.map(&Enum.join/1)
     |> all_primes()
+  end
+
+  def all_primes(slices) do
+    slices
+    |> Enum.filter(&is_prime?/1)
   end
 
   def is_prime?(n) when is_binary(n), do: is_prime?(String.to_integer(n))
@@ -57,14 +42,5 @@ defmodule PrimesPi do
   def calc([h | _t], n) when rem(n, h) == 0, do: false
   def calc([_h | t], n), do: calc(t, n)
   def calc([], _n), do: true
-
-  def slices(_, size) when size <= 0, do: []
-  def slices(s, size) do
-    max = String.length(s)
-    for iterate <- 0..max, iterate + size <= max, do: String.slice(s, iterate, size)
-  end
-
-  def max_digits(numbers) when length(numbers) < @max_digits_constant, do: length(numbers)
-  def max_digits(_), do: @max_digits_constant
 
 end
--- cristineguadelupe/primes_pi/test/primes_pi_test.exs
+++ cristineguadelupe/primes_pi/test/primes_pi_test.exs
@@ -1,21 +1,16 @@
 defmodule PrimesPiTest do
   use ExUnit.Case
 
-  test "all primes - returns all primes " do
-    list1 = [7, 9, 3, 2, 3] |> prepare_slices()
-    list2  = [5, 9, 2, 6, 5] |> prepare_slices()
-    challenge = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6] |> prepare_slices()
-
-    assert PrimesPi.all_primes(list1) == ["9323", "79", "23", "7", "3", "2", "3"]
-    assert PrimesPi.all_primes(list2) == ["59", "5", "2", "5"]
-    assert PrimesPi.all_primes(challenge) == ["4159", "5897", "9323", "653", "41", "59", "53", "89", "97", "79", "23", "5", "2", "5", "3", "5", "7", "3", "2", "3"]
-
+  test "all ordered primes - return all primes in the right order" do
+    challenge = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6] |> prepare_ordered_slices()
+    assert PrimesPi.all_primes(challenge) == ["41", "4159", "5", "59", "2", "653", "5", "53", "3", "5", "5897", "89", "97",
+    "7", "79", "9323", "3", "2", "23", "3"]
   end
 
-  def prepare_slices(list_of_digitis) do
+  def prepare_ordered_slices(list_of_digitis) do
     list_of_digitis
     |> Enum.join()
-    |> PrimesPi.all_slices()
+    |> PrimesPi.prepare_combinations()
   end
 
 end
