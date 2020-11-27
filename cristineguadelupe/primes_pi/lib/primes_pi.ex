d38666c16bff4802e2419540ef7070a45d6fc6d9
Cristine Guadelupe <cristineguadelupe@me.com>
2020-11-27 19:10:47 -0300

    First approach - needs a huge refactor and reorganization. There's also perfomance problems, but at least I got the logic

--- cristineguadelupe/primes_pi/lib/primes_pi.ex
+++ cristineguadelupe/primes_pi/lib/primes_pi.ex
@@ -1,40 +1,59 @@
 defmodule PrimesPi do
 
-  def biggest_sequence(number) when is_binary(number) do
-    number
-    |> prepare_combinations()
-  end
-
   def biggest_sequence(number) do
     number
-    |> Enum.join()
-    |> prepare_combinations()
+    |> all_primes()
+    |> Enum.with_index()
+    |> remove_dups([])
+    |> clear(all_primes(number))
+    |> String.myers_difference(number)
+    |> Keyword.get_values(:eq)
+    |> Enum.max_by(&String.length/1)
   end
 
-  def prepare_combinations(number) do
+  def all_primes(number) do
+    number
+    |> as_string()
+    |> number_to_slices()
+    |> Enum.map(&slice_to_combinations/1)
+    |> Enum.map(&filter_primes/1)
+    |> List.flatten()
+  end
+
+  def number_to_slices(number) do
     number
     |> String.graphemes()
     |> Stream.unfold(fn n -> {Enum.take(n, 4), tl(n)} end)
     |> Enum.take(String.length(number))
-    |> Enum.map(&generate_combinations/1)
-    |> List.flatten()
   end
 
-  def generate_combinations(number) do
-    number
+  def slice_to_combinations(slice) do
+    slice
     |> Enum.reverse()
     |> Stream.unfold(fn n -> {Enum.take(n, length(n)), tl(n)} end)
-    |> Enum.take(length(number))
+    |> Enum.take(length(slice))
     |> Enum.reverse()
     |> Enum.map(&Enum.reverse/1)
     |> Enum.map(&Enum.join/1)
-    |> all_primes()
   end
 
-  def all_primes(slices) do
-    slices
+  def filter_primes(combinations) do
+    combinations
     |> Enum.filter(&is_prime?/1)
   end
+
+  def remove_dups([], acc), do: acc |> List.flatten() |> Enum.sort() |> Enum.uniq()
+  def remove_dups(list_of_primes, acc) do
+    current = hd(list_of_primes)
+    primes = all_primes(elem(current, 0))
+    index  = primes |> Enum.find_index(&(&1 == elem(current, 0)))
+    size = primes |> length()
+    list_to_remove = Enum.filter(0..size-1, fn x -> x != index end) |> adjust_indexes(elem(current, 1), index)
+    acc = [list_to_remove | acc]
+    remove_dups(tl(list_of_primes), acc)
+  end
+
+
 
   def is_prime?(n) when is_binary(n), do: is_prime?(String.to_integer(n))
   def is_prime?(n) when n in [2, 3], do: true
@@ -47,5 +66,42 @@ defmodule PrimesPi do
   def calc([h | _t], n) when rem(n, h) == 0, do: false
   def calc([_h | t], n), do: calc(t, n)
   def calc([], _n), do: true
+
+  def as_string(number) when is_binary(number), do: number
+  def as_string(number), do: Enum.join(number)
+
+  def adjust_indexes(indexes, index, _adjust) when index <= 1, do: indexes
+  def adjust_indexes(indexes, index, adjust) do
+    indexes
+    |> Enum.map(&(&1-adjust))
+    |> Enum.map(&(&1+index))
+  end
+
+
+  def clear(indexes, list) do
+    cleaned =
+      indexes
+      |> Enum.reduce(list, &List.replace_at(&2, &1, "0"))
+      |> Enum.filter(&(&1 != "0"))
+
+    cleaned
+    |> special_case([])
+    |> Enum.zip(cleaned)
+    |> Keyword.get_values(:false)
+    |> Enum.join()
+  end
+
+  def special_case([a, b, c | _rest] = list, acc) do
+    last = String.graphemes(a) |> List.last()
+    current = b
+    next = String.graphemes(c) |> List.first()
+    acc = [special_case?(last, current, next) | acc]
+    special_case(tl(list), acc)
+  end
+  def special_case(_, acc), do: [false | acc] |> Enum.reverse() |> (&([false | &1])).()
+
+  def special_case?(a, b, c) do
+    Enum.join([a, c]) == b
+  end
 
 end
--- cristineguadelupe/primes_pi/test/primes_pi_test.exs
+++ cristineguadelupe/primes_pi/test/primes_pi_test.exs
@@ -2,15 +2,14 @@ defmodule PrimesPiTest do
   use ExUnit.Case
 
   test "all ordered primes - return all primes in the right order" do
-    challenge = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6] |> prepare_ordered_slices()
+    challenge = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6]
     assert PrimesPi.all_primes(challenge) == ["41", "4159", "5", "59", "2", "653", "5", "53", "3", "5", "5897", "89", "97",
     "7", "79", "9323", "3", "2", "23", "3"]
   end
 
-  def prepare_ordered_slices(list_of_digitis) do
-    list_of_digitis
-    |> Enum.join()
-    |> PrimesPi.prepare_combinations()
+  test "biggest sequence - return the biggest sequence of primes" do
+    challenge = "14159265358979323846"
+    assert PrimesPi.biggest_sequence(challenge) == "4159265358979323"
   end
 
 end
