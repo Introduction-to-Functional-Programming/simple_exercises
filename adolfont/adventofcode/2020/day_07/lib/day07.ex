911e12fcbce18cfd5b4d84cd706b095d24682c0f
Adolfo Neto <adolfo.usp@gmail.com>
2020-12-13 21:31:25 -0300

    Nothing done yet for Day 07.

---
+++ adolfont/adventofcode/2020/day_07/lib/day07.ex
@@ -0,0 +1,10 @@
+defmodule Day07 do
+  @moduledoc """
+  Advent of Code 2020 - Day 07 - Handy Haversacks 
+  https://adventofcode.com/2020/day/7
+  """
+
+  def do_it do
+    :done
+  end
+end
---
+++ adolfont/adventofcode/2020/day_07/test/day07_test.exs
@@ -0,0 +1,7 @@
+defmodule Day07Test do
+  use ExUnit.Case
+
+  test "Day 7 first test" do
+    assert Day07.do_it() == :done
+  end
+end
