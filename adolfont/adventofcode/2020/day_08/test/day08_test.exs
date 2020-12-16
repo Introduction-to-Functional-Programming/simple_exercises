dab94957f7d77e8ebdef82648a170665ae651f07
Adolfo Neto <adolfo.usp@gmail.com>
2020-12-15 21:03:23 -0300

    Advent of Code 2020 - Day 8 - Task 2 finished.

--- adolfont/adventofcode/2020/day_08/test/day08_test.exs
+++ adolfont/adventofcode/2020/day_08/test/day08_test.exs
@@ -1,7 +1,7 @@
 defmodule Day08Test do
   use ExUnit.Case
 
-  test "Parse list of instructions from a string" do
+  test "Parses a string containing a list of instructions" do
     instructions = """
     acc -13
     jmp +37
@@ -11,12 +11,14 @@ defmodule Day08Test do
     jmp +413
     """
 
-    assert Day08.parse_list_of_instructions_from_string(instructions) ==
+    assert Day08.parse_instructions(instructions) ==
              [acc: -13, jmp: 37, acc: -19, jmp: 1, jmp: 1, jmp: 413]
   end
 
   test "Parse list of instructions from input.txt" do
-    assert Day08.parse_list_of_instructions_from_file("input.txt") |> Enum.take(6) == [
+    input_file = File.read!("input.txt")
+
+    assert Day08.parse_instructions(input_file) |> Enum.take(6) == [
              acc: -13,
              jmp: 37,
              acc: -19,
@@ -39,13 +41,13 @@ defmodule Day08Test do
     acc +6
     """
 
-    assert Day08.execute_instructions(instructions) == {:infinite_loop, 5, 1, {:jmp, -3}}
+    assert Day08.execute_instructions(instructions) == {:infinite_loop, 5, 4}
   end
 
   test "Task 1" do
     instructions = File.read!("input.txt")
 
-    assert Day08.execute_instructions(instructions) == {:infinite_loop, 1939, 458, {:jmp, -83}}
+    assert Day08.execute_instructions(instructions) == {:infinite_loop, 1939, 541}
   end
 
   test "Task 2 - Example" do
@@ -61,13 +63,12 @@ defmodule Day08Test do
     acc +6
     """
 
-    task_2_result =
-      Day08.execute_instructions(instructions)
-      |> Day08.change_instruction(Day08.parse_list_of_instructions_from_string(instructions))
-      |> IO.inspect()
-      |> Day08.execute_instructions()
-      |> IO.inspect()
+    assert Day08.task_2(instructions) == {:ok, 8}
+  end
 
-    assert task_2_result == {:ok, 8}
+  test "Task 2 with MY input file" do
+    instructions = File.read!("input.txt")
+
+    assert Day08.task_2(instructions) == {:ok, 2212}
   end
 end
