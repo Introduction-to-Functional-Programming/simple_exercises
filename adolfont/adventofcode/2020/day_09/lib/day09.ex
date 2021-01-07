51ea8b63095512aeb41ecc88272b6ec931ee2904
Adolfo Neto <adolfo.usp@gmail.com>
2021-01-07 17:00:45 -0300

    Advent of Code - Day 09 - Task 2 by Martin Gausby

--- adolfont/adventofcode/2020/day_09/lib/day09.ex
+++ adolfont/adventofcode/2020/day_09/lib/day09.ex
@@ -60,7 +60,7 @@ defmodule Day09 do
     end
   end
 
-  # Task 1 from Martin Gausby
+  # Task 1 adpated from Martin Gausby's Task 1 in
   # https://github.com/gausby/aoc2020/blob/main/test/aoc2020/day_09_test.exs
 
   def checksum([_ | remaining] = list, preamble_length) do
@@ -80,30 +80,9 @@ defmodule Day09 do
     end
   end
 
-  # if the number is equal to the sum
-  # of two numbers from the preamble (a list of numbers)
-  # returns :ok
-  # otherwise, retuns an error
   defp do_checksum([_], number), do: {:error, {:invalid, number}}
 
   defp do_checksum([first_element | [_ | _] = rest], number) do
-    # great pattern matching above!
-
-    # this is a bit strange because it is a kind of double negation
-
-    #  let rest = [r1, r2, r3, ..., rn]
-    #  The result of the Enum.drop_while/2 below is
-    # [r1, r2, ..., ri]
-    # such that
-    # for all j<=i
-    # rj + first_element != number
-    # So, an empty list returned means that
-    # there are no element of rest such that number is equal
-
-    # while the sum of the first element with another element is DIFFERENT
-    # from the number that we want to be the sum of two numbers from
-    # the preamble,
-
     case find_first_element_with_property_X(rest, number, first_element) do
       :not_found ->
         do_checksum(rest, number)
@@ -113,7 +92,7 @@ defmodule Day09 do
     end
   end
 
-  def find_first_element_with_property_X(rest, number, first_element) do
+  defp find_first_element_with_property_X(rest, number, first_element) do
     do_find_first_element_with_property_X(
       rest,
       fn an_element ->
@@ -130,16 +109,6 @@ defmodule Day09 do
     number == element1 + element2
   end
 
-  # Enum.drop_while "drops elements at the beginning of the"
-  # "enumerable while fun returns a truthy value."
-  # Enum.drop_while([1, 2, 3, 2, 1], fn x -> x < 4 end)
-  # []
-
-  # do_find_first_element_with_property_X/2 started as
-  # a reimplementation of Enum.drop_while/2
-  # returning different values
-  # but now it is different as it needs a function with a different polarity
-
   defp do_find_first_element_with_property_X([], _function) do
     :not_found
   end
@@ -148,6 +117,43 @@ defmodule Day09 do
     case function.(head) do
       true -> :found
       _ -> do_find_first_element_with_property_X(tail, function)
+    end
+  end
+
+  # TASK 2
+  def find_contiguous_set_which_sums_to(list, preamble_length) do
+    scan_for_window(list, task1(list, preamble_length))
+  end
+
+  def find_encryption_weakness(list, preamble_length) do
+    {:ok, result} = find_contiguous_set_which_sums_to(list, preamble_length)
+    Enum.min(result) + Enum.max(result)
+  end
+
+  # Two functions below adapted from
+  # https://github.com/gausby/aoc2020/blob/main/test/aoc2020/day_09_test.exs
+  defp scan_for_window(numbers, target) do
+    initial_state = {:queue.new(), 0, target}
+
+    case Enum.reduce_while(numbers, initial_state, &do_scan/2) do
+      {:ok, [_ | _]} = success -> success
+      {_window, _sum, _target} -> {:error, :not_found}
+    end
+  end
+
+  defp do_scan(value, {window, sum, target}) do
+    case value do
+      _target_found when target == value + sum ->
+        result = :queue.to_list(:queue.in(value, window))
+        {:halt, {:ok, result}}
+
+      _grow_window when target > value + sum ->
+        window = :queue.in(value, window)
+        {:cont, {window, sum + value, target}}
+
+      _shrink_window_and_retry_value when target < value + sum ->
+        {{:value, leaving}, window} = :queue.out(window)
+        do_scan(value, {window, sum - leaving, target})
     end
   end
 end
--- adolfont/adventofcode/2020/day_09/test/day09_test.exs
+++ adolfont/adventofcode/2020/day_09/test/day09_test.exs
@@ -35,4 +35,66 @@ defmodule Day09Test do
     assert Day09.task1(number_list, 25) == 20_874_512
     assert Day09.checksum(number_list, 25) == {:error, {:invalid, 20_874_512}}
   end
+
+  test "Task 2 example" do
+    input = """
+    35
+    20
+    15
+    25
+    47
+    40
+    62
+    55
+    65
+    95
+    102
+    117
+    150
+    182
+    127
+    219
+    299
+    277
+    309
+    576
+    """
+
+    number_list = Day09.processString(input)
+
+    assert Day09.find_contiguous_set_which_sums_to(number_list, 5) ==
+             {:ok, [15, 25, 47, 40]}
+
+    assert Day09.find_encryption_weakness(number_list, 5) == 62
+  end
+
+  test "Task 2 with input provided by Advent of Code" do
+    number_list = Day09.processString(File.read!("input.txt"))
+
+    assert Day09.find_contiguous_set_which_sums_to(number_list, 25) ==
+             {
+               :ok,
+               [
+                 925_549,
+                 966_007,
+                 1_012_779,
+                 1_054_812,
+                 1_220_613,
+                 1_147_519,
+                 1_158_705,
+                 1_163_492,
+                 1_204_640,
+                 1_209_427,
+                 1_215_826,
+                 1_227_012,
+                 1_231_799,
+                 1_344_496,
+                 1_349_283,
+                 1_355_682,
+                 2_086_871
+               ]
+             }
+
+    assert Day09.find_encryption_weakness(number_list, 25) == 3_012_420
+  end
 end
